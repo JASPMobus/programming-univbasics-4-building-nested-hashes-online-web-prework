f9515c7beceb325f347fa9cc388bf299a50a5471
James Mobus <mobusj@live.com>
2019-08-06 22:12:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   { :railroads => { :pieces => 4,
-                    :rent_in_dollars => { :one_day => 1, 
+                    :rent_in_dollars => { :names => 1, 
                                           :two_days => 1.9,
                                           :three_days => 2.75,
                                           :four_days => 3.5

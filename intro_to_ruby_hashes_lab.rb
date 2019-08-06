d9656c89bf5ae4a82ff61322d1ae4c8e18c6f624
James Mobus <mobusj@live.com>
2019-08-06 22:16:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,8 +20,8 @@ def monopoly_with_third_tier
   { :railroads => { :pieces => 4,
                     :rent_in_dollars => { :one_piece_owned => 25, 
                                           :two_pieces_owned => 50,
-                                          :three_days => 2.75,
-                                          :four_days => 3.5
+                                          :three_pieces_owned => 100,
+                                          :four_pieces_owned => 200
                                         },
                     :names => { :north => "North",
                                 :south => "South",
